14fdaeb16422379b431de7af24997e30e94b6fd6
bengris32 <bengris32@protonmail.ch>
2024-03-24 15:17:34 +0000

    duchamp: Configure default PowerOffAlarm driver
    * Set rtc as the default (and only) driver since this is the only
      supported one on 5.10+ devices.
    
    Change-Id: Iccc5627b5947a0c2cebb8ea706a2af731dfcbf42
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -182,6 +182,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
    FrameworksResOverlayDuchamp \
+   PowerOffAlarmOverlayDuchamp \
    SettingsProviderOverlayDuchampPOCO \
    SettingsProviderOverlayDuchampRedmi \
    SettingsResOverlayDuchamp \
